8f6608a706c6f290ad74f302bbe455625508d038
Andreas Emer <andreas@emdec.de>
2012-04-01 13:32:24 +0200

    Initial Commit

---
+++ skin/frontend/default/responsiv/js/script.js
@@ -0,0 +1,44 @@
+document.observe('dom:loaded', function() {
+    
+    var hideSidebarItemsForPhones = function(selector) {
+        var sidebarItems = $$(selector);
+        sidebarItems.each(function(item, index) {
+            if(index > 0) {
+                item.toggleClassName('hidden-phone');
+            }
+        });        
+    }
+    
+    hideSidebarItemsForPhones('.col-left > div');
+    hideSidebarItemsForPhones('.col-right > div');
+    
+    var moreTranslation = Translator.translate('more');
+    var lessTranslation = Translator.translate('less');
+    
+    var sidebarShowMore = $('sidebar-show-more');
+    
+    if(sidebarShowMore) {
+        sidebarShowMore.observe('click', function(ev) {
+            hideSidebarItemsForPhones('.col-left > div');
+            hideSidebarItemsForPhones('.col-right > div');
+            
+            /**
+             * @todo check for availability of Translator Object
+             */
+            this.innerHTML = this.innerHTML  == lessTranslation ? moreTranslation : lessTranslation;
+        });
+    }
+    
+    //navbar toggle navigation visibility
+    var navbarCollapsible;
+    $$('.navbar .btn-navbar').each(function(navbarButton) {
+        navbarButton.observe('click', function(e) {
+            new Event(e).stop();
+            navbarCollapsible = navbarButton.up('.container').select('.nav-collapse')[0];
+            
+            navbarCollapsible.setStyle({
+                height: (parseInt(navbarCollapsible.getStyle('height')) == 0 ? 'auto' : 0)
+            });
+        });
+    });
+});
